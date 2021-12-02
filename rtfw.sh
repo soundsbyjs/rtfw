cb3e3d469713d8f49c60253c6aa826696e7f15f3
soundsbyjs <85450032+soundsbyjs@users.noreply.github.com>
2021-12-01 21:59:30 -0500

    Update rtfw.sh

--- rtfw.sh
+++ rtfw.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
-# change the path to the location of the python file
-# or just copy both to /usr/local/bin
-# or edit your $PATH if ur a real nerd
+# change the path to the location of the python file then
+# sudo mv rtfw.sh /usr/local/bin/rtfw
+# sudo chmod +x /usr/loca/bin/rtfw
+
+# this is not the only way to install it
 
 cd [path to scraper.py]
 python3 scraper.py $1 $2 $3
